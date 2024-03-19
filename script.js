72fbef61143e004a4e5dc40dc1ca4a8293847f57
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-03-19 21:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// program to check leap year
+// program to check leap year
+let year = docu
+function daysOfAYear(year) {
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return 366;
+    } else {
+        return 365;
+    }
+}
+daysOfAYear(year);
